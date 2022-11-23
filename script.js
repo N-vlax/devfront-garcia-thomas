14b59cd36ad07975dbcc14c1865baa491eece0e3
Thomas <thomas30250@gmail.com>
2022-11-23 18:25:20 +0100

    Création de tout les produit en JS

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function Chaussure(img,nom,prix){
+function Chaussure(img,nom,cat,prix){
     this.img = img,
     this.nom = nom,
+    this.cat = cat,
     this.prix = prix,
-
 }
+
+const Chaussure1 = new Chaussure('src/Chaussure 1.webp','Chaussure Forum Low','Originals',130);
+const Chaussure2 = new Chaussure('src/Chaussure 2.webp','Chaussure basse Forum Exhibit','Originals',120);
+const Chaussure3 = new Chaussure('src/Chaussure 3.webp','Chaussure Ultraboost 22','Performance',80);
+const Chaussure4 = new Chaussure('src/Chaussure 4.webp','Chaussure Superstar','Originals',60);
+const Chaussure5 = new Chaussure('src/Chaussure 5.webp','Chaussure Daily 3.0','Sportwear',200);
+const Chaussure6 = new Chaussure('src/Chaussure 6.webp','Chaussure Daily 3.2','Sportwear',200);
+const Chaussure7 = new Chaussure('src/Chaussure 7.webp','Chaussure Trezoid','Originals',130);
+const Chaussure8 = new Chaussure('src/Chaussure 8.webp','Chaussure Forum Low dos','Originals',130);
+const Chaussure9 = new Chaussure('src/Chaussure 9.webp','Chaussure M&M S x ADIDAS','Originals',130);
