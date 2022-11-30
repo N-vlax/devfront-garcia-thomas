a60f87d9eb99ae4f31f58e203a49ac2a4125876a
Thomas <thomas30250@gmail.com>
2022-11-30 18:25:01 +0100

    remplacement page unique produit par page multiple

--- script.js
+++ script.js
@@ -1,39 +1,23 @@
-function Chaussure(img,nom,cat,prix){
+function produit(img,nom,cat,prix){
+    this.id = id,
     this.img = img,
     this.nom = nom,
     this.cat = cat,
     this.prix = prix,
 }
 
-const Chaussure1 = new Chaussure('src/Chaussure 1.webp','Chaussure Forum Low','Originals',130);
-const Chaussure2 = new Chaussure('src/Chaussure 2.webp','Chaussure basse Forum Exhibit','Originals',120);
-const Chaussure3 = new Chaussure('src/Chaussure 3.webp','Chaussure Ultraboost 22','Performance',80);
-const Chaussure4 = new Chaussure('src/Chaussure 4.webp','Chaussure Superstar','Originals',60);
-const Chaussure5 = new Chaussure('src/Chaussure 5.webp','Chaussure Daily 3.0','Sportwear',200);
-const Chaussure6 = new Chaussure('src/Chaussure 6.webp','Chaussure Daily 3.2','Sportwear',200);
-const Chaussure7 = new Chaussure('src/Chaussure 7.webp','Chaussure Trezoid','Originals',130);
-const Chaussure8 = new Chaussure('src/Chaussure 8.webp','Chaussure Forum Low dos','Originals',130);
-const Chaussure9 = new Chaussure('src/Chaussure 9.webp','Chaussure M&M S x ADIDAS','Originals',130);
+const Chaussure1 = new produit('1','src/Chaussure 1.webp','Chaussure Forum Low','Originals',130);
+const Chaussure2 = new produit('2','src/Chaussure 2.webp','Chaussure basse Forum Exhibit','Originals',120);
+const Chaussure3 = new produit('3','src/Chaussure 3.webp','Chaussure Ultraboost 22','Performance',80);
+const Chaussure4 = new produit('4','src/Chaussure 4.webp','Chaussure Superstar','Originals',60);
+const Chaussure5 = new produit('5','src/Chaussure 5.webp','Chaussure Daily 3.0','Sportwear',200);
+const Chaussure6 = new produit('6','src/Chaussure 6.webp','Chaussure Daily 3.2','Sportwear',200);
+const Chaussure7 = new produit('7','src/Chaussure 7.webp','Chaussure Trezoid','Originals',130);
+const Chaussure8 = new produit('8','src/Chaussure 8.webp','Chaussure Forum Low dos','Originals',130);
+const Chaussure9 = new produit('9','src/Chaussure 9.webp','Chaussure M&M S x ADIDAS','Originals',130);
 
-let chaussure = [];
-chaussure.push(Chaussure1,Chaussure2,Chaussure3,Chaussure4,Chaussure5,Chaussure6,Chaussure7,Chaussure8,Chaussure9);
-
-products.forEach(prod=> {
-    listOfProducts +=  ` 
-      <tr class="text-center">
-        <td><img src=${prod.pic} class="img-fluid img-thumbnail w-50"></td>
-        <td class="w-25 align-middle">${prod.name}</td>
-        <td class="w-25 align-middle">${prod.price}€</td>
-        <td class="w-25 align-middle"><button class="btn btn-info">View</button></td>
-      </tr>
-      `   
-  )
- 
-document.getElementById('productList').innerHTML = listOfProducts;
-}
-
-/// TEST ///
-
+let produit = [];
+produit.push(Chaussure1,Chaussure2,Chaussure3,Chaussure4,Chaussure5,Chaussure6,Chaussure7,Chaussure8,Chaussure9);
 
 
 function savePanier(panier) {
